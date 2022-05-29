aed5507d06ffb6ffbca0bca36e7fed383825f87a
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-05-29 13:06:10 -0500

    Js Solved

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,3 +1,13 @@
-exports.palindrome = function(word) {
+exports.palindrome = function palindrome(word) {
+
+        let letters = /\W/ig;
+        word = word.toString().toLowerCase().split(letters).join('')
+      
+        if(word.length === 0 || word.length === 1){
+          return true
+        } else {
+          return (word.slice(0, 1) === word.slice(-1)) && 
+          palindrome(word.slice(1, -1))
+        }
 
 };
